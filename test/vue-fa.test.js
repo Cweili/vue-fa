f827325c2ab5db7a6e6557a24879d5cd1655fbeb
Cweili <3weili@gmail.com>
2019-05-27 23:26:39 +0800

    test: refactor test

--- test/vue-fa.test.js
+++ test/vue-fa.test.js
@@ -7,20 +7,18 @@ import { createRenderer } from 'vue-server-renderer';
 import App from './app';
 
 const renderer = createRenderer();
-const readdir = util.promisify(fs.readdir);
 const readFile = util.promisify(fs.readFile);
 // const writeFile = util.promisify(fs.writeFile);
 
 const resolve = (...paths) => path.resolve(__dirname, ...paths);
 
-test('vue-fa', async () => {
-  const templates = await readdir(resolve('template'));
-  await Promise.all(templates.map(async (templateName) => {
+fs.readdirSync(resolve('template')).forEach((templateName) => {
+  test(`vue-fa ${templateName}`, async () => {
     const template = await readFile(resolve('template', templateName), 'utf8');
     const out = await renderer.renderToString(new Vue(Object.assign({
       template,
     }, App)));
     // await writeFile(resolve('expected', templateName), out.trim(), 'utf8');
     expect(out.trim()).toBe((await readFile(resolve('expected', templateName), 'utf8')).trim());
-  }));
+  });
 });
